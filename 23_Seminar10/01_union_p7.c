048ec7d393f2a30c48803567d6fe234a04920c57
Dmitry Sudarenko <sudar1977@gmail.com>
2026-03-26 16:54:35 +0300

    Seminar10 union example

--- 23_Seminar10/01_union_p7.c
+++ 23_Seminar10/01_union_p7.c
@@ -14,9 +14,27 @@ int main()
     d.bytes[1], d.bytes[2], d.bytes[3]);
     // Number 12345678 in memory is: 78 56 34 12
     uint32_t number = 0x12345678;
-    
+
     printf(" in memory is: %x\n",(number >> 8)&0xff);
-    
-    
+
+    uint32_t n=0;
+    uint8_t  a=0xaa,b=0xbb,c=0xcc,d=0xdd;
+    n = a;//n=0xaa
+    n = (n<<8) | b;//n=0xaa00 | 0xbb = 0xaabb
+    n = (n<<8) | c;// 0xaabb00| 0xcc = 0xaabbcc
+    n = (n<<8) | d;
+    printf("n = %x\n", n);
+    // n = aabbccdd
+
+    union intbytes n1;
+    n1.bytes[3] = a;//n=0xaa
+    n1.bytes[2] = b;//n=0xaa00 | 0xbb = 0xaabb
+    n1.bytes[1] = c;// 0xaabb00| 0xcc = 0xaabbcc
+    n1.bytes[0] = d;
+    printf("n = %x\n", n1.number);
+    // n = aabbccdd
+
+
+
     return 0;
 }
--- 23_Seminar10/02_union_pi_p8.c
+++ 23_Seminar10/02_union_pi_p8.c
@@ -7,6 +7,22 @@ union intbytes {
     uint8_t bytes[4];
 } d;
 
+
+//Перевод вещественного числа в двоичный вид
+void print_float_bin(union intbytes num) {
+    //~ unsigned int* fi = (unsigned int*)&num;
+    for(int i=31;i>=0;i--) {
+        if(i==30 || i==22)
+            putchar(' ');
+        //~ if(*fi&(1<<i))
+        if(num.number & (1<<i))
+            putchar('1');
+        else
+            putchar('0');
+    }
+    putchar('\n');
+}
+
 int main()
 {
     d.real = 3.14;
@@ -14,5 +30,6 @@ int main()
     printf ("Number %x",d.number);
     printf(" in memory is: %x %x %x %x\n", d.bytes[0],
     d.bytes[1], d.bytes[2], d.bytes[3]);
+    print_float_bin(d);
     return 0;
 }
